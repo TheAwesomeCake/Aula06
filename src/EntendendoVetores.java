19cfff47cdf80559fd9f22d74815a8e97d7207e1
TheAwesomeCake <luc45.2004@gmail.com>
2021-03-05 20:15:02 -0300

    Aprendendo a usar vetores

---
+++ src/EntendendoVetores.java
@@ -0,0 +1,14 @@
+
+public class EntendendoVetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int vetor[] = new int[10];
+		vetor[0] = 5;
+		vetor[0] = 10;
+		
+		System.out.println("A primeira posição contém " + vetor[0]);
+		System.out.println("A segunda posição contém " + vetor[1]);
+	}
+
+}
